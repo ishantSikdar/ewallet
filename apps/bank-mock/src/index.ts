8f019df04d63d836ef5763195ed95ff8424b4c53
ishantSikdar <ishant9715@gmail.com>
2024-07-22 20:45:05 +0530

    Add server health point routes

--- apps/bank-mock/src/index.ts
+++ apps/bank-mock/src/index.ts
@@ -2,11 +2,11 @@ import express from 'express';
 import ewalletReqRouter from './route/tokenRoute';
 import { ROUTE_TOKEN } from '@repo/common/route';
 import cors from 'cors';
+import os from 'os';
+import packageJson from '../package.json';
 
 require('dotenv').config();
 require('dotenv').config({ path: `../../.env` });
-
-console.log("Loaded environment variables");
 
 const PORT: number = Number(process.env.PORT);
 
@@ -17,8 +17,54 @@ app.use(cors({
     origin: process.env.BANK_INTERFACE_BASE_URL
 }));
 
+app.get('/', async (req, res) => {
+    try {
+        const memoryUsage = process.memoryUsage();
+
+        const rssMemory = `${(memoryUsage.rss / (1024 * 1024)).toFixed(2)} MB`;
+        const heapTotalMemory = `${(memoryUsage.heapTotal / (1024 * 1024)).toFixed(2)} MB`;
+        const heapUsedMemory = `${(memoryUsage.heapUsed / (1024 * 1024)).toFixed(2)} MB`;
+        const externalMemory = `${(memoryUsage.external / (1024 * 1024)).toFixed(2)} MB`;
+
+        const serverStatus = {
+            status: 'running',
+            uptime: `${process.uptime()} seconds`,
+            memoryUsage: {
+                rss: `${rssMemory}`,
+                heapTotal: `${heapTotalMemory}`,
+                heapUsed: `${heapUsedMemory}`,
+                external: `${externalMemory}`
+            },
+            osInfo: {
+                platform: os.platform(),
+                release: os.release(),
+                cpuArchitecture: os.arch(),
+                totalMemory: `${(os.totalmem() / (1024 * 1024)).toFixed(2)} MB`,
+                freeMemory: `${(os.freemem() / (1024 * 1024)).toFixed(2)} MB`,
+                uptime: `${os.uptime()} seconds`
+            },
+            cors: [
+                process.env.BANK_INTERFACE_BASE_URL,
+            ],
+            nodeVersion: `${process.version}`,
+            expressVersion: `${packageJson.dependencies.express}`,
+            serverConfiguration: {
+                port: PORT
+            },
+            additionalDependencies: packageJson.dependencies,
+        };
+
+        res.status(200).json(serverStatus);
+    } catch (error: any) {
+        console.error(`Error occurred while fetching server information: ${error}`);
+        res.status(500).json({
+            message: `Server error: ${error.message}`
+        });
+    }
+});
+
 app.use(ROUTE_TOKEN, ewalletReqRouter);
 
-app.listen(8081, () => {
+app.listen(PORT, () => {
     console.log("Bank Mock listening to PORT:", PORT);
 });
--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import webHookRouter from './route/webhookRoute';
 import { ROUTE_WEBHOOK } from '@repo/common/route';
+import packageJson from '../package.json';
+import os from 'os';
 
 require('dotenv').config();
 require('dotenv').config({ path: `../../.env` });
-console.log("Loaded environment variables");
 
 const PORT: number = Number(process.env.PORT);
 
@@ -13,6 +14,50 @@ app.use(express.json());
 
 app.use(ROUTE_WEBHOOK, webHookRouter);
 
-app.listen(8080, () => {
+
+app.get('/', async (req, res) => {
+    try {
+        const memoryUsage = process.memoryUsage();
+
+        const rssMemory = `${(memoryUsage.rss / (1024 * 1024)).toFixed(2)} MB`;
+        const heapTotalMemory = `${(memoryUsage.heapTotal / (1024 * 1024)).toFixed(2)} MB`;
+        const heapUsedMemory = `${(memoryUsage.heapUsed / (1024 * 1024)).toFixed(2)} MB`;
+        const externalMemory = `${(memoryUsage.external / (1024 * 1024)).toFixed(2)} MB`;
+
+        const serverStatus = {
+            status: 'running',
+            uptime: `${process.uptime()} seconds`,
+            memoryUsage: {
+                rss: `${rssMemory}`,
+                heapTotal: `${heapTotalMemory}`,
+                heapUsed: `${heapUsedMemory}`,
+                external: `${externalMemory}`
+            },
+            osInfo: {
+                platform: os.platform(),
+                release: os.release(),
+                cpuArchitecture: os.arch(),
+                totalMemory: `${(os.totalmem() / (1024 * 1024)).toFixed(2)} MB`,
+                freeMemory: `${(os.freemem() / (1024 * 1024)).toFixed(2)} MB`,
+                uptime: `${os.uptime()} seconds`
+            },
+            nodeVersion: `${process.version}`,
+            expressVersion: `${packageJson.dependencies.express}`,
+            serverConfiguration: {
+                port: PORT
+            },
+            additionalDependencies: packageJson.dependencies,
+        };
+
+        res.status(200).json(serverStatus);
+    } catch (error: any) {
+        console.error(`Error occurred while fetching server information: ${error}`);
+        res.status(500).json({
+            message: `Server error: ${error.message}`
+        });
+    }
+});
+
+app.listen(PORT, () => {
     console.log("Bank webhook listening to PORT:", PORT);
 });
